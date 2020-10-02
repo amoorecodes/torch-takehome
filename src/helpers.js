1dbc6849d527b2a24e0d446868d787daea986600
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2020-10-01 18:24:04 -0700

    [fix] AAAAAAAA found  the correct API. Back to square one.

--- src/helpers.js
+++ src/helpers.js
@@ -13,53 +13,86 @@ function notify(line, delayed) {
   }
 }
 
-// this function helps you parse the API for the line that has delays
-// API itself return delayed name as a line group and only specifies the exact line in a text message
-function identifyLine([lineGroup], [message]) {
-  // SIR is a line of itself
-  if (lineGroup === "SIR") return lineGroup;
-  // create a list of all lines in group
-  const lines = lineGroup.split("");
-  for (let line of lines) {
-    // console.log(line);
-    // check if line is mentioned in delayed message
-    if (message.includes(`[${line}]`)) return line;
+// this function checks the cache to illiminate repetetive messages
+function checkCache(name, [newDate], [newTime]) {
+  let { delayed, date, time } = delayedLines[name];
+  // console.log("info: ", delayed, date, time);
+  // console.log("new info: ", newDate, newTime);
+  // return delayedLines[line].delayed ? notify(line) : notify(line, "delayed");
+  // check if line is alerady in cache
+  if (delayed) {
+    // check if delay message is recent
+    if (date === newDate && time === newTime) {
+      console.log("it is the same event...");
+    } else {
+      console.log("this is new update");
+      // delayedLines[name][delayed] = true;
+      // delayedLines[name][date] = newDate;
+      // delayedLines[name][time] = newTime;
+    }
+  } else {
+    console.log("adding to cache for the first time...");
+    delayedLines[name].delayed = true;
+    delayedLines[name].date = newDate;
+    delayedLines[name].time = newTime;
+    delayed = true;
+    date = newDate;
+    time = newTime;
   }
+
+  console.log("after checking cache:\n", delayedLines[name]);
 }
 
 // helper function to get status of all subway lines
 
 function checkStatus() {
   axios
-    .get("http://web.mta.info/status/serviceStatus.txt", {
-      headers: {
-        "x-api-key": process.env.API_KEY,
-        "Content-Type": "text/plain",
-      },
-    })
+    .get(
+      "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fsubway-alerts",
+      {
+        headers: {
+          "x-api-key": process.env.API_KEY,
+        },
+        responseType: "arraybuffer",
+      }
+    )
     .then(({ data }) => {
-      // the API returns an xml file, parse it to JSON
-      xml2js.parseString(data, (err, { service }) => {
-        if (err) console.error(err);
-        // extract delayed routes
-        const filtered = service.subway[0].line.filter(({ status }) => {
-          // console.log(line.name);
-          // return status.includes("DELAYS");
-          // for debugging, sometimes there are no delays
-          // which are lies, it is MTA after all
-          return status.includes("PLANNED WORK");
-        });
-        console.log("filtered\n");
-        // console.log(filtered);
-        // return filtered;
-        // for each route, check if it is in a cache or not
-        for (let lineGroup of filtered) {
-          let line = identifyLine(lineGroup.name, lineGroup.text);
-          delayedLines[line] ? notify(line) : notify(line, "delayed");
-        }
+      let feed = feedMessage.decode(data);
+      const filtered = feed.entity.filter((message) => {
+        // if ID has NYCT it is a delay report
+        return message.id.includes("NYCT");
       });
+      console.log("filtered", filtered.length);
+      for (let message of filtered) {
+        console.log(
+          "checkStatus: ",
+          message.alert.informedEntity[0].trip.routeId
+        );
+      }
+      console.log(filtered[0]);
     })
     .catch(console.err);
+
+  // the API returns an xml file, parse it to JSON
+  //   xml2js.parseString(data, (err, { service }) => {
+  //     if (err) console.error(err);
+  //     // extract delayed routes
+  //     const filtered = service.subway[0].line.filter(({ status }) => {
+  //       // console.log(line.name);
+  //       // return status.includes("DELAYS");
+  //       // for debugging, sometimes there are no delays
+  //       // which are lies, it is MTA after all
+  //       return status.includes("PLANNED WORK");
+  //     });
+  //     // return filtered;
+  //     // for each route, check if it is in a cache or not
+  //     for (let { name, Date, Time, text } of filtered) {
+  //       // console.log("linegroup: ", lineGroup);
+  //       let line = identifyLine(name, text);
+  //       checkCache(line, Date, Time);
+  //     }
+  //   });
+  // })
 }
 
 // cache object with delayed lines
@@ -87,13 +120,17 @@ const subwayLines = [
   "Q",
   "R",
   "S",
-  "SIR",
+  "SI",
 ];
 const delayedLines = {};
 
 function createLinesCache() {
   for (let item of subwayLines) {
-    delayedLines[item] = false;
+    delayedLines[item] = {
+      alerts: [],
+      delayed: false,
+      activePeriod: false,
+    };
   }
 }
 
